ee859459e6df3dc2b8146c8730b5a4a2b028d605
dev-yeon <nochi@kakao.com>
2023-03-05 16:25:16 +0900

    third - can't resolve css T.T

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,67 @@
+package com.cos.photogramstart.domain.user;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+// JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API 를 제공)
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "MyUser") // 디비에 테이블을 생성
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE) // 번호 증가 전략이 데이터베이스를 따라간다.
+	private int id;
+	
+	@Column(length = 100,  unique = true) // OAuth2 로그인을 위해 칼럼 늘리기
+	private String username; 
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String name;
+	private String website; // 웹 사이트
+	private String bio; // 자기 소개
+	@Column(nullable = false)
+	private String email;
+	private String phone;
+	private String gender;
+	
+	private String profileImageUrl; // 사진
+	private String role; // 권한
+	
+	 // 나는 연관관계의 주인이 아니다. 그러므로 테이블에 칼럼을 만들지마.
+	// User 를 Select 할 때 해당 User id로 등록된 image 들을 다 가져와
+	// Lazy = User 를 Select 할 때 해당 User id로 등록된 image 들을 가져오지마 - 대신 getImages() 함수의 image 들이 호출될 때 가져와!!
+	// Eager = User 를 Select 할 때 해당 User id로 등록된 image 들을 전부 Join 해서 가져와!!
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	@JsonIgnoreProperties({"user"})
+	private List<Image> images; // 양방향 매핑
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist // 디비에 INSERT 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", name=" + name + ", website="
+				+ website + ", bio=" + bio + ", email=" + email + ", phone=" + phone + ", gender=" + gender
+				+ ", profileImageUrl=" + profileImageUrl + ", role=" + role +", createDate="
+				+ createDate + "]";
+	}
+
+	
+	
+}
